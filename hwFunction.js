026402046800b8c9b484e41b4c4759328a968256
DmitryMartynenko <vbnz.mart@gmail.com>
2019-12-25 00:01:46 +0300

    fourth

--- hwFunction.js
+++ hwFunction.js
@@ -164,5 +164,49 @@ function funLittle() {
 funLittle()
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+// 5) Write a function stringExpansion
+// Given a string that includes alphanumeric characters ('3a4B2d') return the expansion of that string:
+// The numeric values represent the occurance of each letter preceding that numeric value.
+// There should be no numeric characters in the final string. Empty strings should return an empty string.
+//     The first occurance of a numeric value should be the number of times each character behind it is repeated,
+//     until the next numeric value appears.
+// stringExpansion('3D2a5d2f') === 'DDDaadddddff'
+// If there are two consecutive numeric characters the first one is ignored.
+// stringExpansion('3d332f2a') === 'dddffaa'
+// If there are two consecutive alphabetic characters then the first character has no effect on the one after it.
+// stringExpansion('abcde') === 'abcde'
+// Your code should be able to work for both lower and capital case letters.
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+function stringExpansions(str) {
+    var result = "";
+    var charMultiplier = 1;
+    for (var i = 1; i<str.length;i++) {
+        if (isNumber(str.charAt(i))) {
+            charMultiplier = str.charAt(i);
+        } else {
+            result = appendCharacter(result, str.charAt(i), charMultiplier);
+            charMultiplier = 1;
+        }
+    }
+    return result;
+}
+
+function isNumber(character) {
+    return character >= "0" && character <= "9";
+}
+
+function appendCharacter(string, character, n) {
+    var tmpString = string;
+    for (var i = 0; i < n; i++) {
+        tmpString = tmpString + character;
+    }
+    return tmpString;
+}
+var x = "Ok, i`m w8ing you here";
+stringExpansions(x);
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
